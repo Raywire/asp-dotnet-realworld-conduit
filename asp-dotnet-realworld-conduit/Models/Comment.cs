97612022d3c435098e4e451531584f1158d50fa4
Ryan Wire <ryanwire@outlook.com>
2021-05-15 18:43:54 +0300

    Creates Comment model and migrations

--- asp-dotnet-realworld-conduit/Models/Comment.cs
+++ asp-dotnet-realworld-conduit/Models/Comment.cs
@@ -1,10 +1,30 @@
 ﻿using System;
+using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+
 namespace Conduit.Models
 {
     public class Comment
     {
-        public Comment()
-        {
-        }
+        [Key]
+        public Guid Id { get; set; }
+
+        [MaxLength(256)]
+        [Required]
+        public string Body { get; set; }
+
+        public DateTime CreatedAt { get; set; }
+
+        public DateTime? UpdatedAt { get; set; }
+
+        [ForeignKey("AuthorId")]
+        public User Author { get; set; }
+
+        public Guid AuthorId { get; set; }
+
+        [ForeignKey("ArticleId")]
+        public Article Article { get; set; }
+
+        public Guid ArticleId { get; set; }
     }
 }
