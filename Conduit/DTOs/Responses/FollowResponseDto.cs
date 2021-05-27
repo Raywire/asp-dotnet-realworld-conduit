5c1da537f7b99747f2924b7d2c49d8a8453b7d1a
Ryan Wire <ryanwire@outlook.com>
2021-05-27 22:57:24 +0300

    Renames asp-dotnet-realworld-conduit to Conduit

--- Conduit/DTOs/Responses/FollowResponseDto.cs
+++ Conduit/DTOs/Responses/FollowResponseDto.cs
@@ -6,10 +6,13 @@ namespace Conduit.DTOs.Responses
     public class FollowingResponseDto
     {
         public Guid FollowingId { get; set; }
+        // public AuthorResponseDto Follower { get; set; }
     }
 
     public class FollowerResponseDto
     {
         public Guid FollowerId { get; set; }
+
+        // public AuthorResponseDto Follower { get; set; }
     }
 }
